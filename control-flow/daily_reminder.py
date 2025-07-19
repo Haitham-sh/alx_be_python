89cff7cdb598ce871627284254ea8e0d2c3c8af5
Haitham-sh <haithamelsherbiny81@gmail.com>
2025-07-19 17:15:23 +0300

    task Personal Daily Reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -11,21 +11,29 @@ match priority:
         kind_of_reminder = "Reminder"
         if time_bound == "yes":
             massage = " that requires immediate attention today!"
-        else:
+        elif time_bound == "no":
             massage = ". Consider completing it when you have free time."
+        else:
+            massage = ". Please specify if it is time-bound or not."
+        print(f"{kind_of_reminder}: '{task}' is a {priority} priority{massage}")
     case "medium":
         kind_of_reminder = "Reminder"
         if time_bound == "yes":
             massage = " that requires immediate attention today!"
-        else:
+        elif time_bound == "no":
             massage = ". Consider completing it when you have free time."
+        else:
+            massage = ". Please specify if it is time-bound or not."
+        print(f"{kind_of_reminder}: '{task}' is a {priority} priority{massage}")
     case "low":
         kind_of_reminder = "Note"
         if time_bound == "yes":
             massage = " that requires immediate attention today!"
-        else:
+        elif time_bound == "no":
             massage = ". Consider completing it when you have free time."
+        else:
+            massage = ". Please specify if it is time-bound or not."
+        print(f"{kind_of_reminder}: '{task}' is a {priority} priority{massage}")
     case _:
         print("Invalid priority. Please enter high, medium, or low.")
-
-print(f"{kind_of_reminder}: '{task}' is a {priority} priority{massage}")
+        

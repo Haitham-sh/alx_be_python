4e950dcfa74047eb6b41c147d1b3f1efec28b6c8
Haitham-sh <haithamelsherbiny81@gmail.com>
2025-07-19 17:20:09 +0300

    task Personal Daily Reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -8,32 +8,32 @@ time_bound = input("Is it time-bound? (yes/no): ").lower()
 
 match priority:
     case "high":
-        kind_of_reminder = "Reminder"
+        reminder = "Reminder"
         if time_bound == "yes":
             massage = " that requires immediate attention today!"
         elif time_bound == "no":
             massage = ". Consider completing it when you have free time."
         else:
             massage = ". Please specify if it is time-bound or not."
-        print(f"{kind_of_reminder}: '{task}' is a {priority} priority{massage}")
+        print(f"{reminder}: '{task}' is a {priority} priority{massage}")
     case "medium":
-        kind_of_reminder = "Reminder"
+        reminder = "Reminder"
         if time_bound == "yes":
             massage = " that requires immediate attention today!"
         elif time_bound == "no":
             massage = ". Consider completing it when you have free time."
         else:
             massage = ". Please specify if it is time-bound or not."
-        print(f"{kind_of_reminder}: '{task}' is a {priority} priority{massage}")
+        print(f"{reminder}: '{task}' is a {priority} priority{massage}")
     case "low":
-        kind_of_reminder = "Note"
+        reminder = "Note"
         if time_bound == "yes":
             massage = " that requires immediate attention today!"
         elif time_bound == "no":
             massage = ". Consider completing it when you have free time."
         else:
             massage = ". Please specify if it is time-bound or not."
-        print(f"{kind_of_reminder}: '{task}' is a {priority} priority{massage}")
+        print(f"{reminder}: '{task}' is a {priority} priority{massage}")
     case _:
         print("Invalid priority. Please enter high, medium, or low.")
         

47714a93d5929e24c1a2fdf4b251c9a427150dce
Haitham-sh <haithamelsherbiny81@gmail.com>
2025-08-02 20:48:24 +0300

    solve project Create a Simple Bank Account Class

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -13,4 +13,4 @@ class BankAccount:
         return False
 
     def display_balance(self):
-        print(f"Current Balance: ${self.balance}")
+        print(f"Current Balance: ${float(self.balance):.2f}")

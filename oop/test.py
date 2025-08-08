855ac3dfe28da47cbbe9fe2f95bccae33e5b0a6b
Haitham-sh <haithamelsherbiny81@gmail.com>
2025-08-08 18:04:50 +0300

    solve project Dive into Python Magic Methods

---
+++ oop/test.py
@@ -0,0 +1,16 @@
+ - [Got]
+1984 by George Orwell, published in 1949
+Book('1984' , George Orwell , 1949)
+Deleting 1984
+
+(91 chars long)
+[stderr]: 
+(0 chars long)
+[Expected]
+1984 by George Orwell, published in 1949
+Book('1984', 'George Orwell', 1949)
+Deleting 1984
+
+(91 chars long)
+[stderr]: [Anything]
+(0 chars long)
